c0b3a4a594b7cdb8a5ce709edd7a5f55e2c00ddb
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-24 11:34:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,9 @@
 var marks = [35,38,42,45,43,34,46,41,48,32];
 
 
-function setup() {
+function setup() 
+{
   createCanvas(400,400);
- 
-  for(var i = 0; i<marks.length; i++)
-  { 
-    if(marks[i]>= 45)
-    {
-      console.log(marks[i])
-    }
-  }
 
 }
 
